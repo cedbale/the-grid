4064e4abc6f573f3f43a138e4d02d7d4cf623d1b
Cédric <cedbale@gmail.com>
2016-09-24 17:16:10 +0200

    remove useless function + clean

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const columnsNb = 5;
 let grid = require('./lib/grid');
 let dataGrid = grid.init(linesNb, columnsNb);
 
-console.log('----- colors ------');
+console.log('----- colors ------', '\n');
 dataGrid.forEach((line) => {
     let colors = [];
     line.forEach((square) => {
+        square.colorize();
         colors.push(
-            square.colorize().getColor()
+            square.getColor()
         );
     });
     console.log(colors.join(' | '), '\n');
@@ -21,8 +22,9 @@ console.log('----- status ------', '\n');
 dataGrid.forEach((line) => {
     let colors = [];
     line.forEach((square) => {
+        square.colorize();
         colors.push(
-            square.colorize().getStatus()
+            square.getStatus()
         );
     });
     console.log(colors.join(' | '), '\n');
